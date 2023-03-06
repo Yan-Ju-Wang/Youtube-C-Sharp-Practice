a9c67b7addaf8fd534b5d4a76546d8ec3f67c556
王彥儒 <125424141+Yan-Ju-Wang@users.noreply.github.com>
2023-03-06 17:03:24 +0800

    first commit

---
+++ c-sharp-course-sample-code_小山的 C# 教學所使用的範例程式碼/class-41-50/Class50/Demo/Demo/Resident.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Resident
+    {
+        private String name;
+
+        public Resident(String name)
+        {
+            this.name = name;
+        }
+
+        public override String ToString()
+        {
+            return "住戶 " + name;
+        }
+    }
+}
---
+++ c-sharp-course-sample-code_小山的 C# 教學所使用的範例程式碼/class-41-50/Class50/Demo/Demo/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Worker
+    {
+        private String name;
+
+        public Worker(String name)
+        {
+            this.name = name;
+        }
+
+        public override String ToString()
+        {
+            return "辦公人士 " + name;
+        }
+    }
+}
